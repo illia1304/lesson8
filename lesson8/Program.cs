defb25c9e3f853bda752c628167fca94b619b714
illia1304 <1304illia@gmail.com>
2023-09-24 20:23:39 +0300

    Refactoring of Analyze method

--- lesson8/Program.cs
+++ lesson8/Program.cs
@@ -18,20 +18,11 @@ void Compare(string a, string b)
 void Analyze(string a)
 {
     int numbers = 0, specialCharacters = 0, alp = 0;
-    for(int i = 0; i < a.Length; i++)
+    foreach(char c in a)
     {
-        if(a[i] >= '0' && a[i] <= '9')
-        {
-            numbers++;
-        }
-        else if(a[i] >= 'a' && a[i] <= 'z')
-        {
-            alp++;
-        }
-        else
-        {
-            specialCharacters++;
-        }
+        numbers+=char.IsDigit(c) ? 1: 0;
+        alp += char.IsLetter(c) ? 1: 0;
+        specialCharacters += char.IsSeparator(c) ? 1: 0;
     }
     Console.WriteLine(numbers + " " + alp + " " + specialCharacters);
 }
